888f479eac66fc9f9000bfc58b9f0d7974331f7a
cmkishores <cmkishores@gmail.com>
2019-09-25 20:09:40 +0530

    Setup 2 apps and configured, added templates folder

--- story_website/settings.py
+++ story_website/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    #local apps
+    'stories.apps.StoriesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
